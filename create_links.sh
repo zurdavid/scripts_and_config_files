7bb7d2db465679e288d29a252580be52100e2261
zurdavid <david.z@sunrise.ch>
2021-11-30 18:18:43 +0100

    refactor, fix bug

--- create_links.sh
+++ create_links.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-rm  $HOME/.bashrc $HOME/.zshrc $HOME/.tmux.conf
 # nvim
 rm $HOME/.config/nvim/init.vim
 rm -rf $HOME/.config/nvim/sub
 ln -s $(pwd)/nvim/init.vim $HOME/.config/nvim
 ln -s $(pwd)/nvim/sub $HOME/.config/nvim 
 
+rm $HOME/.bashrc 
+rm $HOME/.inputrc
+rm $HOME/.zshrc
+rm $HOME/.tmux.conf
 ln -s $(pwd)/shell/.bashrc $HOME/.bashrc
 ln -s $(pwd)/shell/.zshrc $HOME/.zshrc
 ln -s $(pwd)/shell/.tmux.conf $HOME/.tmux.conf
