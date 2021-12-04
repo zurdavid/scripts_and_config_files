125fa5dc50ef96d071a331bfc6014bf64d7fab28
zurdavid <david.z@sunrise.ch>
2021-12-04 12:02:17 +0100

    no error if files do not exist

--- create_links.sh
+++ create_links.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
-# nvim
-rm $HOME/.config/nvim/init.vim
+# neovim
+rm -f -- $HOME/.config/nvim/init.vim
 rm -rf $HOME/.config/nvim/sub
 ln -s $(pwd)/nvim/init.vim $HOME/.config/nvim
-ln -s $(pwd)/nvim/sub $HOME/.config/nvim 
+ln -s $(pwd)/nvim/sub $HOME/.config/nvim
 
-rm $HOME/.bashrc 
-rm $HOME/.inputrc
-rm $HOME/.zshrc
-rm $HOME/.tmux.conf
+rm -f -- $HOME/.bashrc
+rm -f -- $HOME/.inputrc
+rm -f -- $HOME/.zshrc
+rm -f -- $HOME/.tmux.conf
 ln -s $(pwd)/shell/.bashrc $HOME/.bashrc
 ln -s $(pwd)/shell/.zshrc $HOME/.zshrc
 ln -s $(pwd)/shell/.tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/shell/.inputrc $HOME/.inputrc
-
