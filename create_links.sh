5ab43d5c1badc644be54a1e5df80c7ef718c4c54
zurdavid <david.z@sunrise.ch>
2022-01-15 23:36:54 +0100

    make sure required folders exist

--- create_links.sh
+++ create_links.sh
@@ -4,10 +4,18 @@ echo "Linking neovim config files..."
 rm -f -- $HOME/.config/nvim/init.vim
 rm -rf $HOME/.config/nvim/sub
 rm -rf $HOME/.config/nvim/ftplugin
+mkdir $HOME/.config/nvim -p
 ln -s $(pwd)/nvim/init.vim $HOME/.config/nvim
 ln -s $(pwd)/nvim/sub $HOME/.config/nvim
 ln -s $(pwd)/nvim/ftplugin $HOME/.config/nvim
 
+
+
+if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]
+then
+  echo "git clone tmux"
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 for dotfile in .bashrc .inputrc .zshrc .tmux.conf .gitconfig ; do
   echo "Linking $dotfile..." 
   rm -f -- $HOME/$dotfile  
