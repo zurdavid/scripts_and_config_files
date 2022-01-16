9cd31b5d363722df76c40fcce9f7a651ed18e785
zurdavid <david.z@sunrise.ch>
2022-01-16 12:30:08 +0100

    download vim-plug

--- create_links.sh
+++ create_links.sh
@@ -9,7 +9,9 @@ ln -s $(pwd)/nvim/init.vim $HOME/.config/nvim
 ln -s $(pwd)/nvim/sub $HOME/.config/nvim
 ln -s $(pwd)/nvim/ftplugin $HOME/.config/nvim
 
-
+# neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]
 then
